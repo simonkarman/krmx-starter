d84e26f8788beaafe95a22ec4122d078c04a3e9f
Simon Karman <mail@simonkarman.nl>
2024-07-14 16:54:54 +0200

    Implemented client side for awesome omelette

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,90 @@
 'use client';
 
 import { Chat } from '@/components/chat';
-import { useClient } from '@/utils/krmx';
+import { client, useClient, useGrid } from '@/utils/krmx';
 import { capitalize } from '@/utils/text';
+import { useEffect, useState } from 'react';
+
+const moves = [
+  { direction: 'up', emoji: '⬆️' },
+  { direction: 'down', emoji: '⬇️' },
+  { direction: 'left', emoji: '⬅️' },
+  { direction: 'right', emoji: '➡️' },
+  { direction: 'none', emoji: '🛑' },
+];
+
+const emptyEmoijs = ['🌱', '🌿', '🍃', '🌾', '🌵', '🌴', '🌳', '🌲'];
+
+const ProgressBar = () => {
+  const { gameLoopInterval, expectedNextGameLoop } = useGrid();
+  const percentage = (expectedNextGameLoop - Date.now()) / gameLoopInterval * 100;
+  const [ , rerender] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      rerender((i: number) => i + 1);
+    }, 50);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+    <div className="h-2.5 rounded-full bg-blue-600" style={{ 'width': `${percentage.toFixed()}%` }}/>
+  </div>;
+};
 
 export default function MyApp() {
   const { status, username } = useClient();
+  const { width, height, cells, currentMove } = useGrid();
   if (status !== 'linked') {
     return null;
   }
 
+  const cellsPerRow = cells.reduce<{ content: string }[][]>((acc, next) => {
+    if (acc[acc.length - 1].length === width) {
+      acc.push([]);
+    }
+    acc[acc.length - 1].push(next);
+    return acc;
+  }, [[]]);
+
   return <div className="m-4">
     <h1 className="text-lg font-bold">Welcome, {capitalize(username!)}!</h1>
     <Chat/>
+    <div className='mx-auto max-w-md space-y-4'>
+      {width === 0 && height === 0
+        ? <p>Loading...</p>
+        : <p className='w-full text-right text-xs'>Grid size: {width}x{height}</p>
+      }
+      <div className='flex flex-col gap-2'>
+        {cellsPerRow.map((row, y) => <div key={y} className='flex items-center justify-between gap-2'>
+          {row.map((cell, x) => {
+            const { content } = cell;
+            return <div
+              key={x}
+              className={'flex w-1/5 grow aspect-square items-center justify-center rounded-lg border border-gray-200 bg-gray-100 p-4 text-center ' +
+                'dark:border-gray-600 dark:bg-gray-800 hover:bg-gray-200 dark:hover:bg-gray-700'}
+            >
+              {content === 'empty'
+                ? emptyEmoijs[(y * width + x) % emptyEmoijs.length]
+                : content.startsWith('player:') ? content.substring('player:'.length).substring(0, 3) : content}
+            </div>;
+          })}
+        </div>)}
+      </div>
+      <ProgressBar />
+      <div className='flex justify-between gap-4'>
+        {moves.map(({ direction, emoji }) => <button
+          onClick={() => client.send({ type: 'ao/move', payload: direction })}
+          key={direction}
+          className={`rounded-lg border-2 px-2 py-1.5 text-4xl transition-transform ${currentMove === direction
+            ? 'border-gray-800 bg-gray-700 dark:border-white dark:bg-gray-200'
+            : 'border-gray-200 bg-gray-100 dark:border-gray-700 dark:bg-gray-800'
+          }`}
+        >
+          {emoji}
+        </button>)}
+      </div>
+    </div>
   </div>;
 }
--- client/src/utils/krmx.ts
+++ client/src/utils/krmx.ts
@@ -2,16 +2,68 @@ import { createClient, createStore } from '@krmx/client-react';
 
 // Create the client
 export const { client, useClient } = createClient();
+client.on('message', console.info);
 
 // Create an example store for messages received from the server
+let id = 0;
 export const useMessages = createStore(
   client,
   /* the initial internal state */
-  [] as { id: string, username: string, text: string }[],
+  [] as { id: number, username: string, text: string }[],
   /* a reducer handling state changes based on incoming messages */
   (state, message) => {
     if (message.type !== 'chat/messaged') return state;
-    return [...state, { ...(message.payload as { username: string, text: string }), id: crypto.randomUUID() }].slice(-10);
+    id += 1;
+    return [...state, { ...(message.payload as { username: string, text: string }), id }].slice(-10);
+  },
+  /* a mapper that can map the internal state to a different format */
+  s => s,
+);
+
+// Create grid store for grid state
+export const useGrid = createStore(
+  client,
+  /* the initial internal state */
+  {
+    currentMove: 'none',
+    cells: [] as { content: string }[],
+    width: 0,
+    height: 0,
+    expectedNextGameLoop: new Date().getTime() + 5000,
+    gameLoopInterval: 5000,
+  },
+  /* a reducer handling state changes based on incoming messages */
+  (state, message) => {
+    if (message.type === 'ao/grid-size') {
+      const { width, height } = message.payload as { width: number, height: number };
+      return {
+        expectedNextGameLoop: 0,
+        gameLoopInterval: 100,
+        width,
+        height,
+        currentMove: 'none',
+        cells: Array.from({ length: height * width }, (_, i) => ({ content: 'empty' })),
+      };
+    }
+    if (message.type === 'ao/grid') {
+      return {
+        ...state,
+        currentMove: 'none',
+        cells: message.payload as { content: string }[],
+      };
+    }
+    if (message.type === 'ao/game-loop') {
+      const interval = message.payload as number;
+      return {
+        ...state,
+        expectedNextGameLoop: Date.now() + interval,
+        gameLoopInterval: interval,
+      };
+    }
+    if (message.type === 'ao/move-confirmed') {
+      return { ...state, currentMove: message.payload as string };
+    }
+    return state;
   },
   /* a mapper that can map the internal state to a different format */
   s => s,
