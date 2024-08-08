305b93024c6e46d3f8ca2096c2c2a192dfa717bb
Simon Karman <mail@simonkarman.nl>
2024-08-08 13:33:27 +0200

    Removed awesome omelette specific code to merge improvements to main branch

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,95 +1,18 @@
 'use client';
 
 import { Chat } from '@/components/chat';
-import { client, useClient, useGrid } from '@/utils/krmx';
+import { useClient } from '@/utils/krmx';
 import { capitalize } from '@/utils/text';
-import { useEffect, useState } from 'react';
-
-const moves = [
-  { direction: 'up', emoji: '⬆️' },
-  { direction: 'down', emoji: '⬇️' },
-  { direction: 'left', emoji: '⬅️' },
-  { direction: 'right', emoji: '➡️' },
-  { direction: 'none', emoji: '🛑' },
-];
-
-const emptyEmoijs = ['🌱', '🌿', '🍃', '🌾', '🌵', '🌴', '🌳', '🌲'];
-
-const ProgressBar = () => {
-  const { gameLoopInterval, expectedNextGameLoop } = useGrid();
-  const zeroOffset = 10;
-  const percentage = Math.max(0, (expectedNextGameLoop - Date.now()) / gameLoopInterval * 100 - zeroOffset) / (100 - zeroOffset) * 100;
-  const [ , rerender] = useState(0);
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      rerender((i: number) => i + 1);
-    }, 100);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
-
-  return <div className="h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
-    <div className="h-2.5 rounded-full bg-blue-600" style={{
-      'width': `${percentage.toFixed()}%`,
-      'transition': 'linear',
-      'transitionDuration': percentage > 99 ? '0ms' : '150ms',
-    }}/>
-  </div>;
-};
 
 export default function MyApp() {
   const { status, username } = useClient();
-  const { width, height, cells, currentMove } = useGrid();
   if (status !== 'linked') {
     return null;
   }
 
-  const cellsPerRow = cells.reduce<{ content: string }[][]>((acc, next) => {
-    if (acc[acc.length - 1].length === width) {
-      acc.push([]);
-    }
-    acc[acc.length - 1].push(next);
-    return acc;
-  }, [[]]);
-
   return <div className="m-4">
     <Chat/>
-    <div className='mx-auto max-w-md space-y-4'>
-      <h1 className="text-lg font-bold">Welcome, {capitalize(username!)}!</h1>
-      <div className='flex flex-col gap-2'>
-        {cellsPerRow.map((row, y) => <div key={y} className='flex items-center justify-between gap-2'>
-          {row.map((cell, x) => {
-            const { content } = cell;
-            return <div
-              key={x}
-              className={'flex w-1/5 grow aspect-square items-center justify-center rounded-lg border border-gray-200 bg-gray-100 p-4 text-center ' +
-                'dark:border-gray-600 dark:bg-gray-800 hover:border-gray-300 dark:hover:border-gray-500'}
-            >
-              {content === 'empty'
-                ? emptyEmoijs[(y * width + x) % emptyEmoijs.length]
-                : content.startsWith('player:') ? content.substring('player:'.length).substring(0, 3) : content}
-            </div>;
-          })}
-        </div>)}
-      </div>
-      {width === 0 && height === 0
-        ? <p>Loading...</p>
-        : <p className='w-full text-right text-xs'>Grid size: {width}x{height}</p>
-      }
-      <ProgressBar/>
-      <div className='flex justify-between gap-4'>
-        {moves.map(({ direction, emoji }) => <button
-          onClick={() => client.send({ type: 'ao/move', payload: direction })}
-          key={direction}
-          className={`rounded-full border-4 bg-gray-100 px-2 py-1.5 text-4xl transition-transform dark:bg-gray-800 ${currentMove === direction
-            ? 'border-blue-500 dark:border-blue-500'
-            : 'border-gray-100 dark:border-gray-800'
-          }`}
-        >
-          {emoji}
-        </button>)}
-      </div>
-    </div>
+    <h1 className="text-lg font-bold">Welcome, {capitalize(username!)}!</h1>
+    <p>There is nothing here yet...</p>
   </div>;
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -5,112 +5,14 @@ const props: Props = { /* configure here */ };
 const server = createServer(props);
 enableOfflineKicker(server);
 
-interface Tile {
-  content: string;
-}
-
-type Move = 'up' | 'down' | 'right' | 'left' | 'none'
-
-const gridWidth = 5;
-const gridHeight = 5;
-const worldGrid: Tile[] = [];
-const gameLoopTime: number = 5000;
-let moves: Record<string, Move> = {};
-const playerGridIndices: Record<string, number> = {};
-const direction: Record<Move, number> = {
-  'up': -gridWidth,
-  'down': gridWidth,
-  'left': -1,
-  'right': 1,
-  'none': 0,
-};
-
-// Set up the world grid, by default all empty
-for (let i = 0; i < gridWidth; i++) {
-  for (let j = 0; j < gridHeight; j++) {
-    const tile: Tile = { content: 'empty' };
-    worldGrid.push(tile);
-  }
-}
-
-server.on('leave', (username) => {
-  worldGrid[playerGridIndices[username]].content = 'empty';
-  server.broadcast({ type: 'ao/grid', payload: worldGrid });
-});
-
-server.on('join', (username) => {
-  const playerGridIndex = worldGrid.findIndex(w => w.content == 'empty');
-  playerGridIndices[username] = playerGridIndex;
-  worldGrid[playerGridIndex].content = `player:${username}`;
-
-  server.broadcast({ type: 'ao/grid', payload: worldGrid }, username);
-});
-
-server.on('link', (username) => {
-  server.send(username, { type: 'ao/grid-size', payload: { width: gridWidth, height: gridHeight } });
-  server.send(username, { type: 'ao/grid', payload: worldGrid });
-
-});
-
 server.on('message', (username, message) => {
   if (message.type === 'chat/message') {
     server.broadcast({ type: 'chat/messaged', payload: { username, text: message.payload } });
   }
-  if (message.type === 'ao/move') {
-    moves[username] = message.payload as Move;
-    server.send(username, { type: 'ao/move-confirmed', payload: message.payload });
-  }
 });
 
-function doGameLoop() {
-  if (server.getStatus() !== 'listening') {
-    return;
-  }
-
-  for (const userName in moves) {
-    const move = moves[userName];
-    const playerGridIndex = playerGridIndices[userName];
-    if (playerGridIndex === undefined) {
-      console.log('move found for non existing player' + userName);
-      continue;
-    }
-
-    //Outside grid, do not move
-    const modulesRemainder = playerGridIndex % gridWidth;
-    if ((move === 'up' && playerGridIndex - gridWidth < 0)
-      || (move === 'down' && playerGridIndex + gridWidth >= worldGrid.length)
-      || (move === 'left' && modulesRemainder === 0)
-      || (move === 'right' && modulesRemainder === (gridWidth - 1))) {
-      console.log(`${userName} moves outside grid, do nothing`);
-      server.send(userName, { type: 'ao/move-denied', payload: 'out-of-grid' });
-      continue;
-    }
-
-    // Is tile available?
-    const targetIndex = playerGridIndex + direction[move];
-    if (worldGrid[targetIndex].content === 'empty') {
-      worldGrid[playerGridIndex].content = 'empty';
-      playerGridIndices[userName] += direction[move];
-      worldGrid[targetIndex].content = `player:${userName}`;
-      console.log(`player ${userName} moved to ${playerGridIndex}`);
-    } else {
-      server.send(userName, { type: 'ao/move-denied', payload: 'obstruction' });
-      continue;
-    }
-  }
-
-  // Reset the moves of each player
-  moves = {};
-
-  // Broadcast updated the world
-  server.broadcast({ type: 'ao/grid', payload: worldGrid });
-  server.broadcast({ type: 'ao/game-loop', payload: gameLoopTime });
-}
-
-const gameLoopIntervalId = setInterval(doGameLoop, gameLoopTime);
-
 server.listen(8082);
+
 export default async () => {
-  clearInterval(gameLoopIntervalId);
   await server.close();
 };
